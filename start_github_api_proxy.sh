7adbf43fc9abcf98e067eef906b0a8a055bc0ae2
Peng Gao <gaopeng@microsoft.com>
2025-12-25 21:15:16 -0800

    update

--- start_github_api_proxy.sh
+++ start_github_api_proxy.sh
@@ -1,5 +1,5 @@
 # via copilot-api
-npx copilot-api start -p 4000
+# npx copilot-api start -p 4000
 
 # via litellm, the port is 4000 by default
-# uvx --from "litellm[proxy]" litellm -c litellm_config.yaml --port 4000
+uvx --from "litellm[proxy]" litellm -c litellm_config.yaml --port 4000
